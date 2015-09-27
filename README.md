c6f5b32781781d08ce7dd4db35052ce76fda20c3
Chris Rault <chris@headwayrocket.com>
2015-09-27 20:08:59 +0200

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # plg-beans-social
 A simple plugin that adds social sharing options to Beans article layout.
+
+Still to do:
+
+- Add Social meta tags (Facebook, Twitter, etc).
+- Add Pinterest sharing to images.
+- Add Social widgets.
+
+Important note: Still in active development. Not intended for production use.
--- plg-beans-social.php
+++ plg-beans-social.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Social Sharing for Beans
  *
@@ -27,20 +26,38 @@
 
 define( 'PLG_BEANS_SOCIAL_URL', plugin_dir_url( __FILE__ ) );
 
+// Stop here if Beans API is not available.
+if ( !file_exists( get_template_directory() . '/lib/api/init.php' ) )
+	return; // you could build a notice thing here
+
 // Include Beans
-require_once( get_template_directory() . '/lib/init.php' );
+require_once( get_template_directory() . '/lib/api/init.php' );
+
+beans_load_api_components( array(
+	'actions',
+	'html',
+	'term-meta',
+	'post-meta',
+	'image',
+	'wp-customize',
+	'compiler',
+	'uikit',
+	'template',
+	'layout',
+	'widget'
+) );
 
 
 // Enqueue uikit assets
-beans_add_smart_action( 'beans_uikit_enqueue_scripts', 'beans_social_enqueue_assets', 7 );
+beans_add_smart_action( 'beans_uikit_enqueue_scripts', 'beans_social_enqueue_assets' );
 
 function beans_social_enqueue_assets() {
 
 	beans_uikit_enqueue_components( array( 'icon' ) );
 
 	// Add the theme js as a uikit fragment
-	beans_compiler_add_fragment( 'social-sharing-js', PLG_BEANS_SOCIAL_URL . '/assets/js/social-sharing.js', 'js' );
-	beans_compiler_add_fragment( 'social-sharing-css', PLG_BEANS_SOCIAL_URL . '/assets/css/beans-social.css', 'css' );
+	beans_compiler_add_fragment( 'uikit', PLG_BEANS_SOCIAL_URL . 'assets/js/social-sharing.js', 'js' );
+	beans_compiler_add_fragment( 'uikit', PLG_BEANS_SOCIAL_URL . 'assets/css/beans-social.css', 'less' );
 
 }
 
@@ -50,29 +67,29 @@ beans_add_smart_action('beans_post_content_append_markup', 'plg_beans_social_sha
 function plg_beans_social_sharing_output() { ?>
 	<div class="uk-button-group element uk-margin-bottom">
 	  <div class="group facebook-share uk-margin-small-right">
-	    <div class="count facebook-count"></div>
-	    <i class="uk-icon--caret-down"></i>
-	    <button class="uk-button fb"><i class="uk-icon-facebook"></i> facebook</button>
+		<div class="count facebook-count"></div>
+		<i class="uk-icon--caret-down"></i>
+		<button class="uk-button fb"><i class="uk-icon-facebook"></i> facebook</button>
 	  </div>
 	  <div class="group gplus-share uk-margin-small-right">
-	    <div class="count gplus-count"></div>
-	    <i class="uk-icon--caret-down"></i>
-	    <button class="uk-button gp"><i class="uk-icon-google-plus"></i> google-plus</button>
+		<div class="count gplus-count"></div>
+		<i class="uk-icon--caret-down"></i>
+		<button class="uk-button gp"><i class="uk-icon-google-plus"></i> google-plus</button>
 	  </div>
 	  <div class="group linkedin-share uk-margin-small-right">
-	    <div class="count linkedin-count"></div>
-	    <i class="uk-icon--caret-down"></i>
-	    <button class="uk-button linkedin"><i class="uk-icon-linkedin"></i> linkedin</button>
+		<div class="count linkedin-count"></div>
+		<i class="uk-icon--caret-down"></i>
+		<button class="uk-button linkedin"><i class="uk-icon-linkedin"></i> linkedin</button>
 	  </div>
 	  <div class="group twitter-share uk-margin-small-right">
-	    <div class="count twitter-count"></div>
-	    <i class="uk-icon--caret-down"></i>
-	    <button class="uk-button twitter"><i class="uk-icon-twitter"></i> twitter</button>
+		<div class="count twitter-count"></div>
+		<i class="uk-icon--caret-down"></i>
+		<button class="uk-button twitter"><i class="uk-icon-twitter"></i> twitter</button>
 	  </div>
 	  <div class="group pinterest-share">
-	    <div class="count pinterest-count"></div>
-	    <i class="uk-icon--caret-down"></i>
-	    <button class="uk-button pinterest"><i class="uk-icon-pinterest"></i> pinterest</button>
+		<div class="count pinterest-count"></div>
+		<i class="uk-icon--caret-down"></i>
+		<button class="uk-button pinterest"><i class="uk-icon-pinterest"></i> pinterest</button>
 	  </div>
 	</div>
 <?php }
